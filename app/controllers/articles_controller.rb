765001832a4fc7ce02b16cd3feae17755a8a87a6
Ben Sanecki & Alex Wen <pair+bensanex.thatnewwen@devbootcamp.com>
2016-05-25 16:25:52 -0500

    fully functional edits with footnotes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,50 @@
 class ArticlesController < ApplicationController
 
-  
+ def show
+    @article = Article.find(params[:id])
+    @category = @article.category
+  end
+
+  def new
+    @article = Article.new
+    @category = Category.new
+    @categories = Category.all.map {|category| [category.name, category.id] }
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path(@article.category, @article)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    @last_edit = Edit.last.content
+    @edit = Edit.new
+  end
+
+  def update
+    @edit = Edit.new(edit_params.merge(article_id: params[:id], editor_id: 1))
+    if @edit.save
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :category_id)
+    end
+
+    def edit_params
+      params.require(:edit).permit(:content)
+    end
 end
--- db/migrate/20160525160158_create_articles.rb
+++ db/migrate/20160525160158_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title, null:false
-      t.integer :category_id, null:false
+      t.integer :category_id
       t.boolean :featured, default:false
 
       t.timestamps null: false
