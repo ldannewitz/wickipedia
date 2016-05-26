8a3912fbfeb80925d46c6ffd7b3062aa9c9aae9b
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-26 13:16:23 -0500

    Category searching

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,19 @@ class Article < ActiveRecord::Base
   has_many :comments
   has_many :commenters, through: :comments
   validates :title, :category_id, presence:true
+
+  def self.matched_articles(search_term)
+    matches = []
+    # matches << Article.where("lower(title) LIKE ?", "%#{params[:search].downcase}%")
+    # matches << Article.where("lower(title) LIKE ?", "%#{params[:search].downcase}%")
+
+    self.all.each do |article|
+      if article.title.downcase.include?(search_term.downcase)
+        matches << article
+      elsif article.edits.last.content.downcase.include?(search_term.downcase)
+        matches << article
+      end
+      matches
+    end
+  end
 end
