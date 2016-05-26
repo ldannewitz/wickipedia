636575ab167df0f454a95a1d6548fadc11d03e45
Ben Sanecki & Alex Wen <pair+bensanex.thatnewwen@devbootcamp.com>
2016-05-26 10:29:52 -0500

    add basic comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
   def index
+    @article = Article.find(params[:id])
     @comments = Comment.all
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params.merge(commenter_id: session[:user_id], article_id: params[:id]))
+
+    if @comment.save
+      redirect_to article_comments_path(params[:category],params[:id])
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
