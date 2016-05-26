44130a53240528cc609f449f6de2670cea060daf
BenSanex <ben.sanecki@gmail.com>
2016-05-26 16:45:15 -0500

    heroku 100%

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,15 +1,15 @@
 // $(document).ready(function(){
-
+//
 //   $("body").on("click",".article-link a", function(event){
 //     event.preventDefault();
 //     var blah = $(this);
 //     var route = $(this).attr("href");
-
+//
 //     var ajaxRequest = $.ajax({
 //       method: "GET",
 //       url: route
 //     });
-
+//
 //     ajaxRequest.done(function(partial){
 //       $(".content").empty();
 //       $(".content").append(partial);
