2d9d2ed9cc6360f8bb5c6139da79da5363c5020c
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-26 18:51:59 -0500

    Update model tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :articles
 
-  def published
+  def published_articles
     self.articles.where(published: true)
   end
-  
+
   def self.matched_categories(search_term)
     matches = self.where("lower(name) LIKE ?", "%#{search_term.downcase}%")
   end
