26166e1c783d2ac2f5edf5d18259fc33854c77aa
Lisa Dannewitz <lisa.dannewitz@gmail.com>
2016-05-27 13:11:53 -0500

    Fix welcome#index featured articles test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe WelcomeController do
-	let!(:featured_articles) { Article.where(featured: true) }
+	let!(:featured_articles) { Article.where(featured: true, published: true) }
 
   describe "GET index" do
     it "assigns @featured_articles" do
       get :index
-      # featured_articles = Article.where(featured: true)
-      expect(assigns(:featured_articles)).to eq(featured_articles)
+      expect(assigns(:featured_articles).count).to eq(featured_articles.count)
     end
   end
 end
